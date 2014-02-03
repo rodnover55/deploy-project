2c9e4106d7201da4fb81b23c0dbeb739f1f42421
Melnikov Sergey <me@rnr.name>
2014-02-03 13:59:43 +0700

    Added wp template.

--- recipes/develop.rb
+++ recipes/develop.rb
@@ -1,1 +1,5 @@
 include_recipe 'deploy-project::enviroment'
+case node['deploy-project']['type']
+when 'php-wp'
+  include_recipe 'deploy-project::php-wp'
+end
---
+++ recipes/php-wp.rb
@@ -0,0 +1,12 @@
+db_name = node['deploy-project']['db']['database'] || node['deploy-project']['project']
+
+mysql_database db_name do
+  connection(
+      :host     => node['deploy-project']['db']['host'],
+      :username => node['deploy-project']['db']['user'],
+      :password => node['deploy-project']['db']['password']
+  )
+  not_if do ::File.exists?("#{node['deploy-project']['source']}/wp_config.php") end
+  sql { ::File.open(node['deploy-project']['db']['install']).read }
+  action :query
+end
