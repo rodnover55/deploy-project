ac9e88a7720fd5439a2df872b63d90b158fd7431
Melnikov Sergey <me@rnr.name>
2014-02-05 17:40:04 +0700

    Changed wordpress template.
    В ветке master
    Изменения для закрепления:
    	new file:   recipes/php-oc.rb
    Changes not staged for commit:
    В ветке master
    Изменения для закрепления:
    	new file:   recipes/php-oc.rb
    Changes not staged for commit:

--- recipes/php-wp.rb
+++ recipes/php-wp.rb
@@ -6,13 +6,14 @@ mysql_database db_name do
       :username => node['deploy-project']['db']['user'],
       :password => node['deploy-project']['db']['password']
   )
-  not_if do ::File.exists?("#{node['deploy-project']['path']}/wp-config.php") end
+  not_if { ::File.exists?("#{node['deploy-project']['path']}/wp-config.php") ||
+      !::File.exists?(node['deploy-project']['db']['install']) }
   sql { ::File.open(node['deploy-project']['db']['install']).read }
   action :query
 end
 
 template "#{node['deploy-project']['path']}/wp-config.php" do
-  source 'wp-config.php.erb'
+  source 'wp-wp-config.php.erb'
   owner node['apache']['user']
   group node['apache']['group']
 end
