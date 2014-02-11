081d0e5162e73193ea92e2d83b7817fec35fb03d
Melnikov Sergey <me@rnr.name>
2014-02-11 14:26:35 +0700

    Changed php oc config.

--- recipes/php-oc.rb
+++ recipes/php-oc.rb
@@ -45,6 +45,12 @@ directory "#{node['deploy-project']['path']}/system/cache/" do
   action :create
 end
 
+directory "#{node['deploy-project']['path']}/image/cache/" do
+  owner node['apache']['user']
+  group node['apache']['group']
+  action :create
+end
+
 template "#{node['deploy-project']['path']}/.htaccess" do
   source 'oc-htaccess.erb'
   owner node['apache']['user']
