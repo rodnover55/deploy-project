36bc21f2f6c33a57f9f482b778278b53dec20646
Melnikov Sergey <me@rnr.name>
2014-02-13 15:30:18 +0700

    Fixed typo.

--- recipes/php-cake.rb
+++ recipes/php-cake.rb
@@ -1,10 +1,3 @@
-template "#{node['deploy-project']['path']}/.htaccess" do
-  source 'php-cacke.htaccess.erb'
-  owner node['apache']['user']
-  group node['apache']['group']
-end
-
-
 template "#{node['deploy-project']['path']}/app/Config/core.php" do
   source 'php-cake-core.php.erb'
   owner node['apache']['user']
