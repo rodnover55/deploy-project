c7c43e61ab3e51fd69ca4b32bfc43b72b7cec8ac
Melnikov Sergey <me@rnr.name>
2014-02-13 19:13:43 +0700

    Added default config install type.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['deploy-project']['db']['host'] = '127.0.0.1'
 default['deploy-project']['db']['user'] = 'root'
 default['deploy-project']['db']['password'] = ''
-default['deploy-project']['db']['force-install'] = false
+default['deploy-project']['db']['install_type'] = 'normal'
 default['deploy-project']['php-oc']['base'] = '/'
 default['deploy-project']['db']['provider'] = 'pdo'
 default['php']['modules_conf_dir'] = '/etc/php5/mods-available/'
