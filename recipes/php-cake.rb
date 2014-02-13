7dded10b61c06c529be26b7efb0c9d3dd6430d27
Melnikov Sergey <me@rnr.name>
2014-02-13 16:25:16 +0700

    Added database config.

--- recipes/php-cake.rb
+++ recipes/php-cake.rb
@@ -4,6 +4,12 @@ template "#{node['deploy-project']['path']}/app/Config/core.php" do
   group node['apache']['group']
 end
 
+template "#{node['deploy-project']['path']}/app/Config/database.php" do
+  source 'php-cake-database.php.erb'
+  owner node['apache']['user']
+  group node['apache']['group']
+end
+
 template "#{node['deploy-project']['path']}/.htaccess" do
   source 'php-cake-htaccess.erb'
   owner node['apache']['user']
