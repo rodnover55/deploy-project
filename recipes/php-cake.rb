623289e1a570abd4055d11f837538574726006b2
Melnikov Sergey <me@rnr.name>
2014-02-13 15:23:47 +0700

    Added .htaccess template.
    В ветке master
    Изменения для закрепления:
    	new file:   templates/default/php-cake-webroot-htaccess.erb
    Changes not staged for commit:

--- recipes/php-cake.rb
+++ recipes/php-cake.rb
@@ -12,7 +12,13 @@ template "#{node['deploy-project']['path']}/app/Config/core.php" do
 end
 
 template "#{node['deploy-project']['path']}/.htaccess" do
-  source 'php-cake.htaccess.erb'
+  source 'php-cake-htaccess.erb'
+  owner node['apache']['user']
+  group node['apache']['group']
+end
+
+template "#{node['deploy-project']['path']}/app/webroot/.htaccess" do
+  source 'php-cake-webroot-htaccess.erb'
   owner node['apache']['user']
   group node['apache']['group']
 end
