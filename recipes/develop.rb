5c0b7a17904def45319dd73ff4151d7d5223cdce
Melnikov Sergey <me@rnr.name>
2014-02-14 12:55:26 +0700

    Changed mysql listen interface for develop environment.

--- recipes/develop.rb
+++ recipes/develop.rb
@@ -21,12 +21,22 @@ package 'php5-xdebug'
 ip = node[:network][:interfaces][:eth1][:addresses].detect{|k,v| v[:family] == "inet" }.first
 remote_ip = ip.gsub /\.\d+$/, '.1'
 
-template "#{node['php']['modules_conf_dir']}xdebug.ini" do
-  source 'php-xdebug.ini.erb'
-  owner node['apache']['user']
-  group node['apache']['group']
-  variables({remote_host: remote_ip})
-  notifies :reload, "service[apache2]", :delayed
+needApacheConfigure = !(node['deploy-project']['disabled'] || []).include?('apache-configure')
+needMysqlConfigure = !(node['deploy-project']['disabled'] || []).include?('mysql-configure')
+
+if needApacheConfigure
+  template "#{node['php']['modules_conf_dir']}xdebug.ini" do
+    source 'php-xdebug.ini.erb'
+    variables({remote_host: remote_ip})
+    notifies :reload, "service[apache2]", :delayed
+  end
+end
+
+if needMysqlConfigure
+  template "/etc/mysql/conf.d/custom.cnf" do
+    source 'mysql-custom.cnf.erb'
+    notifies :reload, "service[mysql]", :delayed
+  end
 end
 
 include_recipe 'deploy-project::configure'
