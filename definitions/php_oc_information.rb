6de4f8b17cac8c017ffaf12b27101758f8afef8b
Melnikov Sergey <me@rnr.name>
2014-02-14 17:19:15 +0700

    Changed arguments.

--- definitions/php_oc_information.rb
+++ definitions/php_oc_information.rb
@@ -1,4 +1,7 @@
-define :php_oc_information, :keyword, :template, :title do
+define :php_oc_information, :keyword => nil, :template => nil, :title => nil do
+  if params[:template].nil? || params[:title].nil?
+    raise 'Set required params'
+  end
   params[:template] ||= params[:name]
   execute "php oc template #{params[:title]}" do
     command "php cli/index.php configure/information '#{params[:keyword]}' '#{params[:template]}' '#{params[:title]}'"
