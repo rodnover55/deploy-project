f2cdcd9ac6cdfb59070cfbd8c5628f9b6dc504b2
Melnikov Sergey <me@rnr.name>
2014-02-24 11:37:38 +0700

    Added check information in php oc module.

--- definitions/php_oc_information.rb
+++ definitions/php_oc_information.rb
@@ -1,12 +1,21 @@
 define :php_oc_information, :keyword => nil, :template => nil, :title => nil, :sort_order => 0,
-       :bottom => 1, :status => 1 do
+       :bottom => 1, :status => 1, :force => false do
   if params[:keyword].nil? || params[:title].nil?
     raise 'Set required params'
   end
   params[:template] ||= params[:name]
-  execute "php cli/index.php configure/information '#{params[:keyword]}' '#{params[:template]}' '#{params[:title]}' '#{params[:sort_order]}' '#{params[:bottom]}' '#{params[:status]}'" do
-    command "php cli/index.php configure/information '#{params[:keyword]}' '#{params[:template]}' '#{params[:title]}' '#{params[:sort_order]}' '#{params[:bottom]}' '#{params[:status]}'"
-    cwd node['deploy-project']['path']
-    action :run
+
+  if params[:force]
+    execute "php cli/index.php configure/information '#{params[:keyword]}' '#{params[:template]}' '#{params[:title]}' '#{params[:sort_order]}' '#{params[:bottom]}' '#{params[:status]}'" do
+      command "php cli/index.php configure/information '#{params[:keyword]}' '#{params[:template]}' '#{params[:title]}' '#{params[:sort_order]}' '#{params[:bottom]}' '#{params[:status]}'"
+      cwd node['deploy-project']['path']
+      action :run
+    end
+  else
+    execute "php cli/index.php configure/information_check '#{params[:keyword]}' '#{params[:template]}' '#{params[:title]}' '#{params[:sort_order]}' '#{params[:bottom]}' '#{params[:status]}'" do
+      command "php cli/index.php configure/information '#{params[:keyword]}' '#{params[:template]}' '#{params[:title]}' '#{params[:sort_order]}' '#{params[:bottom]}' '#{params[:status]}'"
+      cwd node['deploy-project']['path']
+      action :run
+    end
   end
 end
