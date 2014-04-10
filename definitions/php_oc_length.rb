4005c8bb10133950c8b058207ea8da0bbd20a7ab
Melnikov Sergey <me@rnr.name>
2014-04-10 16:09:38 +0700

    Fixed length execute.

--- definitions/php_oc_length.rb
+++ definitions/php_oc_length.rb
@@ -16,4 +16,11 @@ define :php_oc_length, :slug => nil, :length_class_id => nil, :value => nil, :de
     description_data.merge!({length_class_id: params[:length_class_id]})
   end
 
+  description = description_data.to_json
+
+  execute "echo '#{description}' | php cli/index.php length/save" do
+    cwd node['deploy-project']['path']
+    action :run
+  end
+
 end
