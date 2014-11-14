60db94876586a53cdf9c175e309d835ca7d938a0
Melnikov Sergey <me@rnr.name>
2014-11-15 01:01:08 +0600

    Fixed typo

--- recipes/develop.rb
+++ recipes/develop.rb
@@ -5,9 +5,9 @@ directory node['deploy-project']['path'] do
   group node['apache']['group']
 end
 
-execute "umount #{node['vdna']['path']}" do
-  not_if "ls -ld '#{node['vdna']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['vdna']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
+execute "umount #{node['deploy-project']['path']}" do
+  not_if "ls -ld '#{node['deploy-project']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['deploy-project']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
 end
-execute "mount -t vboxsf -o rw,uid=`id -u #{node['apache']['user']}`,gid=`id -g #{node['apache']['user']}` #{sharedAlias} #{node['vdna']['path']}" do
-  not_if "ls -ld '#{node['vdna']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['vdna']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
+execute "mount -t vboxsf -o rw,uid=`id -u #{node['apache']['user']}`,gid=`id -g #{node['apache']['user']}` #{sharedAlias} #{node['deploy-project']['path']}" do
+  not_if "ls -ld '#{node['deploy-project']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['deploy-project']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
 end
