e9bae20a3bffe9fa84d016b71979c436f69f0945
Melnikov Sergey <me@rnr.name>
2014-12-16 13:07:52 +0600

    Remounting path was changed.

--- recipes/develop.rb
+++ recipes/develop.rb
@@ -1,8 +1,8 @@
 sharedAlias = 'project_root'
 
-execute "umount #{node['deploy-project']['path']}" do
-  not_if "ls -ld '#{node['deploy-project']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['deploy-project']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
+execute "umount #{node['deploy-project']['repo']['path']}" do
+  not_if "ls -ld '#{node['deploy-project']['repo']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['deploy-project']['repo']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
 end
-execute "mount -t vboxsf -o rw,uid=`id -u #{node['apache']['user']}`,gid=`id -g #{node['apache']['user']}` #{sharedAlias} #{node['deploy-project']['path']}" do
-  not_if "ls -ld '#{node['deploy-project']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['deploy-project']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
+execute "mount -t vboxsf -o rw,uid=`id -u #{node['apache']['user']}`,gid=`id -g #{node['apache']['user']}` #{sharedAlias} #{node['deploy-project']['repo']['path']}" do
+  not_if "ls -ld '#{node['deploy-project']['repo']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['deploy-project']['repo']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
 end
