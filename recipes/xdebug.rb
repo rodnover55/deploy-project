6de30f2ec93dfb411a6cdea08f296d83009a2450
Melnikov Sergey <me@rnr.name>
2015-01-05 20:47:14 +0600

    XDebug path was fixed.

--- recipes/xdebug.rb
+++ recipes/xdebug.rb
@@ -1,9 +1,12 @@
+lib_location = 'xdebug.so'
+
 case node['platform']
   when 'redhat', 'centos', 'fedora'
     package 'php-pecl-xdebug' do
       if not node['deploy-project']['old-php']
         options '--enablerepo=remi-php55,remi'
       end
+      lib_location = '/usr/lib64/php/modules/xdebug.so'
     end
   when 'debian', 'ubuntu'
     package 'php5-xdebug'
@@ -22,7 +25,10 @@ remote_ip = ip.gsub /\.\d+$/, '.1'
 
 template "#{dirConfig}xdebug.ini" do
   source 'php-xdebug.ini.erb'
-  variables({remote_host: remote_ip})
+  variables({
+    remote_host: remote_ip,
+    lib_location: lib_location
+  })
   notifies :reload, 'service[apache2]', :delayed
 end
 
