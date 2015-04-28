9a4370559c462c358e4a91c785ab53b67afcd6dd
Melnikov Sergey <me@rnr.name>
2015-04-28 14:14:33 +0600

    Added ssh forwarding for dev env.

--- recipes/develop.rb
+++ recipes/develop.rb
@@ -6,3 +6,8 @@ end
 execute "mount -t vboxsf -o rw,uid=`id -u #{node['apache']['user']}`,gid=`id -g #{node['apache']['user']}` #{sharedAlias} #{node['deploy-project']['repo']['path']}" do
   not_if "ls -ld '#{node['deploy-project']['repo']['path']}' | awk '{ print $3; }' | grep '#{node['apache']['user']}' && ls -ld '#{node['deploy-project']['repo']['path']}' | awk '{ print $4; }' | grep '#{node['apache']['user']}'"
 end
+
+file 'Add ssh forwarding agent for sudo' do
+    path '/etc/sudoers.d/10-ssh-forward'
+    content "Defaults    env_keep+=SSH_AUTH_SOCK\n"
+end
